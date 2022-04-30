86a54a588e61ac8b2cc891d46da4e7179f9b64ff
Brian Retamar <leo1632@hotmail.com>
2022-04-30 16:36:18 -0300

    Added 'realease date' property to model

--- GintamaArcsScrapper/Models/Arc.cs
+++ GintamaArcsScrapper/Models/Arc.cs
@@ -12,5 +12,6 @@ namespace GintamaArcsScrapper.Models
         public string Url { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
+        public string ReleaseDate { get; set; }
     }
 }
